fa03961358beec43e68fe4b0cf698b21c04f687b
viking <djarums@freemail.hu>
2014-08-18 20:39:11 +0200

    I said restructured filesystem

--- jobSeeker/Job.java
+++
@@ -1,8 +0,0 @@
-package jobSeeker;
-
-public interface Job 
-{
-	public void setKnownLanguages(Languages[] langs) throws Exception;
-    public Languages[] getKnownLanguages();
-    public String getJobType();
-}
--- jobSeeker/Logger.java
+++
@@ -1,9 +0,0 @@
-package jobSeeker;
-
-public class Logger 
-{
-    public static void error(String theErrorMessage)
-    {
-    	System.out.println(theErrorMessage);
-    }
-}
